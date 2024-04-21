3f25bd3a6fdb29081fc05236b59cece874fe6027
real2two <26689496+real2two@users.noreply.github.com>
2024-04-20 23:36:54 -0400

    Added a easier load function for loading images

--- packages/canvas/src/images.ts
+++ packages/canvas/src/images.ts
@@ -3,13 +3,15 @@ import { getDirname } from './utils';
 
 const __dirname = getDirname(import.meta.url);
 
+const load = (file: string) => loadImage(`${__dirname}/images/${file}`);
+
 export default {
   avatars: {
-    '0': await loadImage(`${__dirname}/images/avatars/0.png`),
-    '1': await loadImage(`${__dirname}/images/avatars/1.png`),
-    '2': await loadImage(`${__dirname}/images/avatars/2.png`),
-    '3': await loadImage(`${__dirname}/images/avatars/3.png`),
-    '4': await loadImage(`${__dirname}/images/avatars/4.png`),
-    '5': await loadImage(`${__dirname}/images/avatars/5.png`),
+    '0': await load('avatars/0.png'),
+    '1': await load('avatars/1.png'),
+    '2': await load('avatars/2.png'),
+    '3': await load('avatars/3.png'),
+    '4': await load('avatars/4.png'),
+    '5': await load('avatars/5.png'),
   },
 };
